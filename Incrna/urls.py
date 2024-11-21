72bff5ee183d7100ed460ea370543672e12837aa
Andrew Newman <andrewfnewman@gmail.com>
2024-11-21 17:28:19 +1000

    Create a GET version of the image search.

--- Incrna/urls.py
+++ Incrna/urls.py
@@ -2,12 +2,10 @@ import django.urls as dj_urls
 import geneExplorer.views as views
 
 urlpatterns = [
-    # path('admin/',admin.site.urls),
-    # path('genes', geneExplorer, name = "ganes"),
     dj_urls.path('', views.HomeView.as_view(), name="Ok"),
     dj_urls.path('genes_list', views.GenesListView.as_view(), name="GenesListView"),
     dj_urls.path('genesid', views.GeneIDsListsView.as_view(), name="GeneIDsListsView"),
-    dj_urls.path('genes', views.GeneView.gene_view_api, name='GeneExplorerView'),
+    dj_urls.path('genes', views.GeneView.as_view(), name='GeneExplorerView'),
     dj_urls.path('genesSlr', views.GeneSpatialLongReadView.gene_spatial_long_read_view_api, name='GeneExplorerView'),
     dj_urls.path('genesScr', views.SingleCellView.gene_explorer_view_api, name='SingleCellView'),
 
--- geneExplorer/views/not_found_image.py
+++ geneExplorer/views/not_found_image.py
@@ -4,14 +4,14 @@ import django.conf as django_conf
 
 
 class NotFoundImage:
+    png_path = os.path.join(django_conf.settings.BASE_DIR, 'static/no_data.png')
     _base64_png = None
 
     @staticmethod
     def not_found():
         if NotFoundImage._base64_png is None:
-            png_path = os.path.join(django_conf.settings.BASE_DIR, 'static/no_data.png')
             try:
-                with open(png_path, 'rb') as png_file:
+                with open(NotFoundImage.png_path, 'rb') as png_file:
                     png_data = png_file.read()
                     NotFoundImage._base64_png = base64.b64encode(png_data).decode('utf-8')
             except FileNotFoundError:
